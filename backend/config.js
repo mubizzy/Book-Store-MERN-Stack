d98f96b1328b1dcbb875148dc3557aa34b297bc4
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2024-09-19 08:35:06 +0100

    new info 2

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,11 @@
 // export const PORT = 5555;
 
-// export const mongoDBURL ='mongodb+srv://admin:IMjWeVz6ygPnbI1q@cluster0.tyg8fi3.mongodb.net/?retryWrites=true&w=majority'
+
 
 // // Please create a free database for yourself.
 // // This database will be deleted after tutorial
 
-// // mongodb+srv://admin:IMjWeVz6ygPnbI1q@cluster0.tyg8fi3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 
 // // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
 import dotenv from "dotenv"
